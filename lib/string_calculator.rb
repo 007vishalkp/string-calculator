909cfcfacee41d2812f385604edf4053850dc928
Vishal KP <vishalkp@The-Bat.local>
2025-04-13 20:17:14 +0530

    test: raise error for negative numbers with full list

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,10 +5,16 @@ class StringCalculator
         delimiter = /,|\n/
         if input.start_with?("//")
           delimiter_part, input = input.split("\n", 2)
-          delimiter = Regexp.escape(delimiter_part[2])  # Get custom char
+          delimiter = Regexp.escape(delimiter_part[2])
         end
     
         numbers = input.split(/#{delimiter}/).map(&:to_i)
+        
+        negatives = numbers.select { |n| n < 0 }
+        if negatives.any?
+          raise "Negative numbers not allowed: #{negatives.join(', ')}"
+        end
+    
         numbers.sum
     end
 end
