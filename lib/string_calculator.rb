4de102c2b173886545f57293bd7cc93559c7029b
Vishal KP <vishalkp@The-Bat.local>
2025-04-13 20:14:35 +0530

    test: support newline as delimiter along with commas

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,9 +1,8 @@
 class StringCalculator
     def add(input)
         return 0 if input.empty?
-        return input.to_i unless input.include?(',')
     
-        numbers = input.split(',').map(&:to_i)
+        numbers = input.split(/,|\n/).map(&:to_i)
         numbers.sum
     end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -22,5 +22,10 @@ RSpec.describe StringCalculator do
         expect(calculator.add("1,2,3")).to eq(6)
     end
 
+    it 'supports newline as delimiter along with commas' do
+        calculator = StringCalculator.new
+        expect(calculator.add("1\n2,3")).to eq(6)
+    end
+
   end
 end
